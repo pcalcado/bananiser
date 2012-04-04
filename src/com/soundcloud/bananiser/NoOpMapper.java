cfe36ded7b69d9f7de08524685a64cf9898321b6
Phil Calçado <pcalcado@gmail.com>
2012-04-04 16:31:05 +0200

    mapper needs to specify type or wont run, now sure hot to test this :(

--- src/com/soundcloud/bananiser/NoOpMapper.java
+++ src/com/soundcloud/bananiser/NoOpMapper.java
@@ -5,12 +5,8 @@ import java.io.IOException;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
-import com.soundcloud.bananiser.SeqFileCompatibleMapper;
-
-@SuppressWarnings("rawtypes")
-public class NoOpMapper extends SeqFileCompatibleMapper {
+public class NoOpMapper extends SeqFileCompatibleMapper<Text> {
     @Override
-    @SuppressWarnings("unchecked")
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
         context.write(key, value);
